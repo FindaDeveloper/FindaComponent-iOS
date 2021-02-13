c7fccfd5c85f3c018daae5c4be0d7c6729090f77
ParkYoungJin0303 <dev.parkyoungjin@gmail.com>
2021-02-13 20:54:36 +0900

    feature: Jogak/Header - FindaBasicHeader

---
+++ Source/Jogak/Header/FindaBasicHeader.swift
@@ -0,0 +1,20 @@
+//
+//  FindaBasicHeader.swift
+//  FindaComponent
+//
+//  Created by 박영진 on 2021/02/13.
+//
+
+import UIKit
+
+class FindaBasicHeader: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
