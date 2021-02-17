44c442c7cd47548a34fc10914a02a839f39dc0d3
ParkYoungJin0303 <dev.parkyoungjin@gmail.com>
2021-02-17 15:59:56 +0900

    feature: Jogak/Button - FindaButton

---
+++ Source/Jogak/Button/FindaButton.swift
@@ -0,0 +1,20 @@
+//
+//  FindaButton.swift
+//  FindaComponent
+//
+//  Created by 박영진 on 2021/02/17.
+//
+
+import UIKit
+
+class FindaButton: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
